bc1db53a5c8a81acb6201b0e9f80439a1090c342
tyler fishbone <tyler.fishbone@gmail.com>
2024-03-07 11:55:02 -0600

    capitalize Count

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <h1>Vite + React + Tyler</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
