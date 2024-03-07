93cfd22a724950f213b88364f94812f4e8b57b8e
tyler fishbone <tyler.fishbone@gmail.com>
2024-03-07 11:46:43 -0600

    added self pic to homescreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import tylerPic from './assets/github.jpeg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -15,8 +16,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={tylerPic} className="logo react" alt="Tyler face pic" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Tyler</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
